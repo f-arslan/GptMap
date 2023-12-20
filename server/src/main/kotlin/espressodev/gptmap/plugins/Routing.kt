4bfb839f58427ac37cf135520cbacc713b972fd0
f-arslan <buildsmstff@gmail.com>
2023-12-20 18:52:25 +0300

    refactor: unsplash endpoint fixed

--- server/src/main/kotlin/espressodev/gptmap/plugins/Routing.kt
+++ server/src/main/kotlin/espressodev/gptmap/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package espressodev.gptmap.plugins
 
 import espressodev.gptmap.api.unsplash.UnsplashService
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -10,11 +11,18 @@ import org.koin.ktor.ext.inject
 fun Application.configureRouting() {
     val unsplashService by inject<UnsplashService>()
     routing {
-        get("/") {
-            unsplashService.getTwoPhotos("london").onSuccess {
-                call.respond(it)
-            }.onFailure {
-                call.respond(it)
+        route("/photos") {
+            get("/{query}") {
+                val query = call.parameters["query"] ?: run {
+                    call.respondText("Missing query parameter", status = HttpStatusCode.BadRequest)
+                    return@get
+                }
+                unsplashService.getTwoPhotos(query)
+                    .onSuccess {
+                        call.respond(it)
+                    }.onFailure {
+                        call.respond(HttpStatusCode.InternalServerError, it.message ?: "Unknown error")
+                    }
             }
         }
     }
