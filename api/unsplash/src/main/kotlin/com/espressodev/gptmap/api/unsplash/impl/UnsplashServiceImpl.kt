2560434428ee23642cec16ea33b8ca293b262a84
Fatih Arslan <66749900+f-arslan@users.noreply.github.com>
2024-02-12 22:59:28 +0300

    Refactor UnsplashServiceImpl and UnsplashModule not null classfields became null

--- api/unsplash/src/main/kotlin/com/espressodev/gptmap/api/unsplash/impl/UnsplashServiceImpl.kt
+++ api/unsplash/src/main/kotlin/com/espressodev/gptmap/api/unsplash/impl/UnsplashServiceImpl.kt
@@ -1,12 +1,16 @@
 package com.espressodev.gptmap.api.unsplash.impl
 
-import android.util.Log
 import com.espressodev.gptmap.api.unsplash.UnsplashApi
 import com.espressodev.gptmap.api.unsplash.UnsplashService
 import com.espressodev.gptmap.core.model.unsplash.LocationImage
-import com.espressodev.gptmap.core.model.ext.classTag
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.util.UUID
+
+/**
+ * Even the data class fields are not null response can make your data class fields null
+ * how is this allowed?
+ */
 
 class UnsplashServiceImpl(private val unsplashApi: UnsplashApi) : UnsplashService {
     override suspend fun getTwoPhotos(query: String): Result<List<LocationImage>> =
@@ -15,10 +19,17 @@ class UnsplashServiceImpl(private val unsplashApi: UnsplashApi) : UnsplashServic
             response.isSuccessful.let { success ->
                 when {
                     success -> {
-                        response.body()?.let {
-                            Result.success(it)
+                        response.body()?.let { images ->
+                            val notNullImages = images.map {
+                                it.copy(
+                                    id = UUID.randomUUID().toString(),
+                                    analysisId = ""
+                                )
+                            }
+                            Result.success(notNullImages)
                         } ?: Result.failure(Throwable(UnsplashApiException()))
                     }
+
                     else -> {
                         Result.failure(Throwable(UnsplashApiException()))
                     }
--- api/unsplash/src/main/kotlin/com/espressodev/gptmap/api/unsplash/module/UnsplashModule.kt
+++ api/unsplash/src/main/kotlin/com/espressodev/gptmap/api/unsplash/module/UnsplashModule.kt
@@ -29,7 +29,7 @@ object UnsplashModule {
                 val newRequestBuilder = request.newBuilder()
                     .addHeader("Content-Type", "application/json")
 
-                val token = runBlocking { getFirebaseToken() }.also(::println)
+                val token = runBlocking { getFirebaseToken() }
 
                 if (token != null) {
                     newRequestBuilder.addHeader("Authorization", "Bearer $token")
